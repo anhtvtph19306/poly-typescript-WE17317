4a0d20a1eecdd18f2af5c8df7211c32f9cebdd05
anhtvtph19306 <118867306+anhtvtph19306@users.noreply.github.com>
2023-04-08 11:59:41 +0700

    listSP admin

--- ass2/src/API/auth.ts
+++ ass2/src/API/auth.ts
@@ -2,11 +2,11 @@ import instance from ".";
 import { SinginForm, SingupForm } from "../models";
 
 export const singup = (data: SingupForm)=>{
-    const uri = "/users"
+    const uri = "/singup"
     return instance.post(uri, data)
 }
 
 export const singin = (data: SinginForm)=>{
-    const uri = "/users"
+    const uri = "/singin"
     return instance.post(uri, data)
 }
--- ass2/src/App.tsx
+++ ass2/src/App.tsx
@@ -1,21 +1,28 @@
 import { useState } from 'react'
-import {BrowserRouter, Router, Routes, Route} from 'react-router-dom'
-import UserLayout from './components/userlayout'
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
+import UserLayout from './components/layout/userlayout'
 import Home from './pages/home'
 import ProductDetail from './pages/product'
 import Singup from './pages/singup'
 import Singin from './pages/singin'
+import AdminLayout from './components/layout/admin'
+import Dashboard from './pages/dashboard'
+
+
 
 function App() {
 
   return <BrowserRouter>
     <Routes>
-      <Route path='/singup' element={<Singup/>} />
-      <Route path='/singin' element={<Singin/>} />
+      <Route path='/singup' element={<Singup />} />
+      <Route path='/singin' element={<Singin />} />
 
-      <Route path='/' element={<UserLayout/>}> {/* Layout */}
-        <Route index element={<Home/>}/>
-        <Route path='product/:id' element={<ProductDetail/>}/>
+      <Route path='/' element={<UserLayout />}> {/* Layout */}
+        <Route index element={<Home />} />
+        <Route path='product/:id' element={<ProductDetail />} />
+      </Route>
+      <Route path='/admin' element={<AdminLayout/>}>
+        <Route index element={<Dashboard/>} />
       </Route>
     </Routes>
   </BrowserRouter>
