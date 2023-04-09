07ec8f35c0993bd076165c7099d570ace17a006f
anhtvtph19306 <118867306+anhtvtph19306@users.noreply.github.com>
2023-04-09 17:13:12 +0700

    flowbite

--- ass2/tailwind.config.cjs
+++ ass2/tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts,tsx,js,jsx}"],
+  content: ["./src/**/*.{html,ts,tsx,js,jsx}","./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
